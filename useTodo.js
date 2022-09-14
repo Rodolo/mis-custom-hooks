280ab83e1c303c5f499725dd28bcc41acc4654df
Jon Mateos <jonmateos8@gmail.com>
2022-09-14 19:36:28 +0200

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,60 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos') || []);
+}
+
+
+const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+    
+  
+
+    const onNewTodo = (todo) => {
+        console.log("onNewTodo", {todo});
+    
+        const addTodoAction = {
+          type: 'add',
+          payload: todo,
+        }
+    
+        dispatch(addTodoAction);
+      };
+    
+      const onDeleteTodo = (id) => {
+        console.log("onDeleteTodo", id);
+    
+        const deleteTodoAction = {
+          type: 'delete',
+          payload: id,
+        }
+      
+        dispatch(deleteTodoAction);
+      }
+    
+      const onToggleTodo = (id) => {
+        console.log("onToggleTodo", id);
+    
+      const toggleTodoAction = {
+          type: 'toggle',
+          payload: id
+        }
+    
+        dispatch(toggleTodoAction);
+    
+    
+      }
+      
+
+      return {todos, onNewTodo, onDeleteTodo, onToggleTodo};
+}
+
+export default useTodo
